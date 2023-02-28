486af06da785fb21867fbb6050eda30600bdbeb4
Geri <ggasconmoline@gmail.com>
2023-02-28 21:17:21 +0100

    Tiles

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -38,7 +38,7 @@ public class GameManager : MonoBehaviour {
 		OrbTypes selected;
 		Array values = Enum.GetValues(typeof(OrbTypes));
 		do {
-			selected = (OrbTypes)values.GetValue(Random.Range(0, values.Length));
+			selected = (OrbTypes)values.GetValue(Random.Range(1, values.Length));
 		} while (selected == currentType || selected == _lastType);
 		
 		ChangeEventCall(selected);
